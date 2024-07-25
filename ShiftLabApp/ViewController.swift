f465d5b69973dc414be93f243f257ab9040cf0ba
sivkaburka47 <164678325+sivkaburka47@users.noreply.github.com>
2024-07-25 21:12:32 +0500

    Initial commit

---
+++ ShiftLabApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShiftLabApp
+//
+//  Created by Станислав Дейнекин on 25.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
